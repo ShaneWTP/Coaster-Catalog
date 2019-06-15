fc39812e0dc209a387d419c504dfa4541e305a62
savycodr <46404719+savycodr@users.noreply.github.com>
2019-06-15 12:37:09 -0400

    begin authentication structure

---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+
+class LoginForm extends Component {
+	constructor() {
+		super()
+		this.state = {
+			username: '',
+			password: '',
+			redirectTo: null
+		}
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	handleSubmit(event) {
+		event.preventDefault()
+		console.log('handleSubmit')
+		// this.props._login(this.state.username, this.state.password)
+
+
+
+		axios
+			.get('/signin', {
+				username: this.state.username,
+				password: this.state.password
+			})
+			.then(response => {
+				console.log(response)
+				if (!response.data.errmsg) {
+					console.log('youre good')
+					this.setState({
+						redirectTo: '/'
+					})
+				} else {
+					console.log('duplicate')
+				}
+			})
+
+
+		this.setState({
+			redirectTo: '/'
+		})
+	}
+
+	render() {
+		if (this.state.redirectTo) {
+			return <Redirect to={{ pathname: this.state.redirectTo }} />
+		} else {
+			return (
+				<div className="LoginForm">
+					<h1>Login form</h1>
+					<form>
+						<label htmlFor="username">Username: </label>
+						<input
+							type="text"
+							name="username"
+							value={this.state.username}
+							onChange={this.handleChange}
+						/>
+						<label htmlFor="password">Password: </label>
+						<input
+							type="password"
+							name="password"
+							value={this.state.password}
+							onChange={this.handleChange}
+						/>
+						<button onClick={this.handleSubmit}>Login</button>
+					</form>
+				</div>
+			)
+		}
+	}
+}
+
+export default LoginForm
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +12,40 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+//The following lines are for authentication
+// var db = require("./models");
+var passport   = require('passport');
+var session    = require('express-session');
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+var bodyParser = require('body-parser');
+// The flash is a special area of the session used for storing messages. Messages are written to the flash and cleared after being displayed to the user.
+// this allows a message to be flashed to the user in a session
+var flash = require('connect-flash');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+// For using secure cookies in production, but allowing for testing in development
+// var sess = {
+//   secret: 'keyboard cat',
+//   cookie: {}
+// }
+// if (app.get('env') === 'production') {
+//   app.set('trust proxy', 1) // trust first proxy
+//   sess.cookie.secure = true // serve secure cookies
+// }
+// app.use(session(sess));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions(calls the deserializer)
+app.use(flash()); // for session messaging
+
+// Passport Strategy for authentication. Pass in passport and User object
+// require("./config/passport/passport.js")(passport, db.User);
+require("./config/passport/passport.js")(passport);
+// Route for authentication
+require("./routes/authRoutes")(app, passport);
+// end authentication
+
 // Define API routes here
 
 // Send every other request to the React app
@@ -18,6 +53,10 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/coasterdb");
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
