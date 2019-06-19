18361a1c296ec05857b30e71126de223b62f4a5e
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-06-19 15:02:51 -0400

    map fix 2

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,7 +6,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg">
             <a className="navbar-brand" href="#">
-                <img src={logo} alt="logo" />
+                <img className="navbar-logo" src={logo} alt="logo" />
             </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
