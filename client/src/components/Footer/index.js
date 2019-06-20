39f912ebd5b7b11f5ecb19de46563ba0c847186a
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-06-19 20:40:14 -0400

    footer revision

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,10 +5,30 @@ import "./style.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="bottom">
-        {/* eslint-disable-next-line react/jsx-no-target-blank */}
-        Coaster Catalog <i className="fab fa-react"></i> &nbsp;  &copy; Copyright 2019 <a href="#" target="_blank" className="footer-text">The Four Loops</a>. All rights reserved. <a href="#" target="_blank" className="footer-text"> See Project Repo <i className="fas fa-external-link-alt"/></a> 
-      </div>  
+      <div className="container">
+        <div className="row footer-row">
+          <div className="col l4 s12">
+            <h5 className>About COASTER CATALOG™</h5>
+            <p>Our Team creates interative roller coaster catalogs that provide roller coaster enthusiasts with the best on-the-ground, real-world information so they can choose what their next great thrill-ride is going to be and what to expect when they get there</p>
+          </div>
+          <div className="col l4 s12">
+            <h5 className>Coaster Catalog™</h5>
+            <p>Coaster Catalog™ provides roller coaster enthusiasts with the best on-the-ground, real-world information so they can choose what their next great thrill-ride is going to be and what to expect when they get there</p>
+          </div>
+          <div className="col l4 offset-l2 s12">
+            <h5>Created with ♥ by The Four Loops</h5>
+            <ul>
+              <li><a className="footer-text" href="https://github.com/savycodr"><i class="fab fa-github"></i> Heather Samsel</a></li>
+              <li><a className="footer-text" href="https://github.com/jenerationx"><i class="fab fa-github"></i> Jenny Sterlein</a></li>
+              <li><a className="footer-text" href="https://github.com/ShaneWTP"><i class="fab fa-github"></i> Shane Maziekien</a></li>
+              <li><a className="footer-text" href="https://github.com/swissfink"><i class="fab fa-github"></i> Richard Fink</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <div className="container">© 2019 Copyright The Four Loops</div>
+      </div>
     </footer>
   );
 }
