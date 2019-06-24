77b8e059f3fb9bccd3673005be95c2b4b1cde4c5
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-06-24 17:57:40 -0400

    coaster section test 5

--- client/src/components/CoasterCard/index.js
+++ client/src/components/CoasterCard/index.js
@@ -5,42 +5,40 @@ import "./style.css";
 const CoasterCard = props => {
   return (
 
-    <div className ="container">
+    <div className="container">
       {props.coasters.map(coaster => {
         return (
-          <div className="cc-section">
-            <li className="card my-1 coastercard" key={coaster._id}>
-              <Row className="row" id={coaster._id} >
-                <Col size="1">
-                  <div className="color-div" id={coaster.park.split(" ")[0]} ></div>
-                </Col>
-                <Col size="3">
-                  <a href={"/coasters/" + coaster._id}>
-                    <div className="coasterImage my-3">
-                      <img src={coaster.img1 || "https://via.placeholder.com/150C/O"} className="img-circle" alt={coaster.name} />
-                    </div></a>
-                </Col>
-                <Col size="md-8" className="coasterInfo px-3">
-                  <div className="card-block px-3">
-                    <Row>
-                      <a href={"/coasters/" + coaster._id} className="nameLink">
-                        <h4 className="coasterName card-title text-left my-2">{coaster.name}</h4>
-                      </a>
-                    </Row>
-                    <Row>
-                      <p className="coasterPark text-left">{coaster.park} | {coaster.location}</p>
-                    </Row>
-                    <Row>
-                      <p className="coasterRating">{coaster.rating}</p>
-                    </Row>
-                    <Row>
-                      <button className="btn btn-success btn-md" id={coaster._id} onClick={(event) => props.handleNewCoasterSubmit(event)}>I Rode this</button>
-                    </Row>
-                  </div>
-                </Col>
-              </Row>
-            </li>
-          </div>  
+          <li className="card my-1 coastercard cc-section" key={coaster._id}>
+            <Row className="row" id={coaster._id} >
+              <Col size="1">
+                <div className="color-div" id={coaster.park.split(" ")[0]} ></div>
+              </Col>
+              <Col size="3">
+                <a href={"/coasters/" + coaster._id}>
+                  <div className="coasterImage my-3">
+                    <img src={coaster.img1 || "https://via.placeholder.com/150C/O"} className="img-circle" alt={coaster.name} />
+                  </div></a>
+              </Col>
+              <Col size="md-8" className="coasterInfo px-3">
+                <div className="card-block px-3">
+                  <Row>
+                    <a href={"/coasters/" + coaster._id} className="nameLink">
+                      <h4 className="coasterName card-title text-left my-2">{coaster.name}</h4>
+                    </a>
+                  </Row>
+                  <Row>
+                    <p className="coasterPark text-left">{coaster.park} | {coaster.location}</p>
+                  </Row>
+                  <Row>
+                    <p className="coasterRating">{coaster.rating}</p>
+                  </Row>
+                  <Row>
+                    <button className="btn btn-success btn-md" id={coaster._id} onClick={(event) => props.handleNewCoasterSubmit(event)}>I Rode this</button>
+                  </Row>
+                </div>
+              </Col>
+            </Row>
+          </li>
         );
       })}
     </div>
