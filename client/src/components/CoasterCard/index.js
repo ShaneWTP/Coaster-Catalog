c217b0b3dfd244bfd91f59a90a0830e2c6882034
Shane Maziekien <shane.maziekien9@gmail.com>
2019-06-25 20:21:04 -0400

    mega menu population

--- client/src/components/CoasterCard/index.js
+++ client/src/components/CoasterCard/index.js
@@ -12,7 +12,7 @@ const CoasterCard = props => {
         <div className="col l12 s12" id="map-title">
           <h3>Roller Coaster Index</h3>
           <br/>
-          <p>Page through the list for information about all the roller coasters in Pennsylvania are located. <br/>Click on a Coaster's image to go to the Coaster's Profile page for even more infromation about the coaster's origin and stats.</p>
+          <p>Page through the list for information about all the roller coasters in Pennsylvania are located. <br/>Click on a Coaster's image to go to the Coaster's Profile page for even more information about the coaster's origin and stats.</p>
           <br/>
         </div> 
       </div>  
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -53,14 +53,14 @@ class Navbar extends Component {
       <MDBNavbar color="green darken-3" dark expand="md">
         <MDBNavbarBrand>
           <MDBNavLink to="/">
-              <div className="logo-container">
-                <img src={logo} width="550" height="85" className="d-inline-block align-top" alt="logo" />
-                <div className="overlay">
-                  <a href="/" className="image">
-                    <img src={LogoHover} width="550" height="85" className="d-inline-block align-top" alt="logo-hover" />
-                  </a>
-                </div>
+            <div className="logo-container">
+              <img src={logo} width="550" height="85" className="d-inline-block align-top" alt="logo" />
+              <div className="overlay">
+                <a href="/" className="image">
+                  <img src={LogoHover} width="550" height="85" className="d-inline-block align-top" alt="logo-hover" />
+                </a>
               </div>
+            </div>
           </MDBNavLink>
         </MDBNavbarBrand>
         <MDBNavbarToggler onClick={this.toggleCollapse} />
@@ -75,66 +75,82 @@ class Navbar extends Component {
                   <div class="row p-3 drop-down-card">
                     <div class="col-4">
                       <span>Camelback Mountain Adventures</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1a">Appalachian Express Mountian Coaster</MDBDropdownItem>
                       <span>Conneaut Lake Park</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e04">Blue Streak</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e09">Devil's Den</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e17">Little Dipper</MDBDropdownItem>
                       <span>DelGrosso's Amusement Park</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e08">Crazy Mouse</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e29">Wacky Worm</MDBDropdownItem>
                       <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
                       <span>Dorney Park and Wildwater Kingdom</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dfc">Hydra The Revenge</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dfd">Possessed</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dfe">Steel Force</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e23">Talon</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e25">Thunderhawk</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e2d">Wild Mouse</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e2e">Woodstock's Express</MDBDropdownItem>
                       <span>Dutch Wonderland</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                    </div>            
-                    <div class="col-4">
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e10">Joust</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e11">Kingdom Coaster</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e19">Merlin's Mayhem</MDBDropdownItem>
                       <span>Fun Fore All Family Entertainment Center</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <span>Hersheypark</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <span>Idlewild and SoakZone</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <span>Kennywood</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <span>Knoebels Amusement Park and Resort</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0c">Fiesta Express</MDBDropdownItem>
                     </div>
                     <div class="col-4">
+                      <span>Hersheypark</span>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e06">Cocoa Cruiser</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e05">Comet</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0b">Fahrenheit</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0e">Great Bear</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e14">Laff Trakk</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e15">Lightning Racer</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dfb">Sidewinder</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257df9">Skyrush</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e21">SooperDooperLooper</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dfa">Storm Runner</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e26">Trailblazer</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e2a">Wildcat</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e2c">Wild Mouse</MDBDropdownItem>
+                      <span>Idlewild and SoakZone</span>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e20">Rollo Coaster</MDBDropdownItem>
+                      <MDBDropdownItem href="/coaster/5d0aae19c64c9f1682257e2b">Wild Mouse</MDBDropdownItem>
+                      <span>Kennywood</span>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0a">Exterminator</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dff">Jack Rabbit</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e16">Lil' Phantom</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1c">Phantom's Revenge</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e00">Racer</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e01">Sky Rocket</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e02">Steel Curtain</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e24">Thunderbolt</MDBDropdownItem>
+                    </div>
+                    <div class="col-4">
+                      <span>Knoebels Amusement Park and Resort</span>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e03">Black Diamond</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0d">Flying Turns</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0f">Impulse</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e12">Kozmo's Kurves</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1d">Phoenix</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e27">Twister</MDBDropdownItem>
                       <span>Lakemont Park</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e13">Leap-The-Dips</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e18">Little Leaper</MDBDropdownItem>
                       <span>Sesame Place</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1b">Oscar's Wacky Taxi</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e28">Vapor Trail</MDBDropdownItem>
                       <span>Waldameer</span>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Coaster 1</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e07">Comet</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1f">Ravine Flyer II</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1e">Ravinie Flyer III</MDBDropdownItem>
+                      <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e22">Steel Dragon</MDBDropdownItem>
                     </div>
                   </div>
                 </MDBDropdownMenu>
               </MDBDropdown>
-            </MDBNavItem> 
+            </MDBNavItem>
             <MDBNavItem className="profile">
               <MDBNavLink to="/userprofile">Profile</MDBNavLink>
             </MDBNavItem>
