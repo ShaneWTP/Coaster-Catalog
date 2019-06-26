ca0e52350bb6a3ab8736a546de233f21cdceaaab
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-06-26 10:28:14 -0400

    navbar and some homepage styling

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -54,10 +54,10 @@ class Navbar extends Component {
         <MDBNavbarBrand>
           <MDBNavLink to="/">
             <div className="logo-container">
-              <img src={logo} width="550" height="85" className="d-inline-block align-top" alt="logo" />
+              <img src={logo} width="400" height="60" className="d-inline-block align-top" alt="logo" />
               <div className="overlay">
                 <a href="/" className="image">
-                  <img src={LogoHover} width="550" height="85" className="d-inline-block align-top" alt="logo-hover" />
+                  <img src={LogoHover} width="400" height="60" className="d-inline-block align-top" alt="logo-hover" />
                 </a>
               </div>
             </div>
@@ -68,22 +68,22 @@ class Navbar extends Component {
           <MDBNavbarNav left>
             <MDBNavItem className="parks">
               <MDBDropdown>
-                <MDBDropdownToggle nav caret className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <MDBDropdownToggle nav caret className="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <div className="d-none d-md-inline">Parks &amp; Coasters</div>
                 </MDBDropdownToggle>
                 <MDBDropdownMenu className="dropdown-default" aria-labelledby="dropdownMenuButton">
                   <div className="row p-3 drop-down-card">
                     <div className="col-4">
-                      <span>Camelback Mountain Adventures</span>
+                      <span className="park-name">Camelback Mountain Adventures</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1a">Appalachian Express Mountian Coaster</MDBDropdownItem>
-                      <span>Conneaut Lake Park</span>
+                      <span className="park-name">Conneaut Lake Park</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e04">Blue Streak</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e09">Devil's Den</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e17">Little Dipper</MDBDropdownItem>
-                      <span>DelGrosso's Amusement Park</span>
+                      <span className="park-name">DelGrosso's Amusement Park</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e08">Crazy Mouse</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e29">Wacky Worm</MDBDropdownItem>
-                      <span>Dorney Park and Wildwater Kingdom</span>
+                      <span className="park-name">Dorney Park and Wildwater Kingdom</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dfc">Hydra The Revenge</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dfd">Possessed</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dfe">Steel Force</MDBDropdownItem>
@@ -91,15 +91,15 @@ class Navbar extends Component {
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e25">Thunderhawk</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e2d">Wild Mouse</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e2e">Woodstock's Express</MDBDropdownItem>
-                      <span>Dutch Wonderland</span>
+                      <span className="park-name">Dutch Wonderland</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e10">Joust</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e11">Kingdom Coaster</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e19">Merlin's Mayhem</MDBDropdownItem>
-                      <span>Fun Fore All Family Entertainment Center</span>
+                      <span className="park-name">Fun Fore All Family Entertainment</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0c">Fiesta Express</MDBDropdownItem>
                     </div>
                     <div class="col-4">
-                      <span>Hersheypark</span>
+                      <span className="park-name">Hersheypark</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e06">Cocoa Cruiser</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e05">Comet</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0b">Fahrenheit</MDBDropdownItem>
@@ -113,34 +113,34 @@ class Navbar extends Component {
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e26">Trailblazer</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e2a">Wildcat</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e2c">Wild Mouse</MDBDropdownItem>
-                      <span>Idlewild and SoakZone</span>
+                      <span className="park-name">Idlewild and SoakZone</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e20">Rollo Coaster</MDBDropdownItem>
                       <MDBDropdownItem href="/coaster/5d0aae19c64c9f1682257e2b">Wild Mouse</MDBDropdownItem>
-                      <span>Kennywood</span>
+                      <span className="park-name">Kennywood</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0a">Exterminator</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257dff">Jack Rabbit</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e16">Lil' Phantom</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1c">Phantom's Revenge</MDBDropdownItem>
+                    </div>
+                    <div className="col-4">
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e00">Racer</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e01">Sky Rocket</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e02">Steel Curtain</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e24">Thunderbolt</MDBDropdownItem>
-                    </div>
-                    <div className="col-4">
-                      <span>Knoebels Amusement Park and Resort</span>
+                      <span className="park-name">Knoebels Amusement Park</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e03">Black Diamond</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0d">Flying Turns</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e0f">Impulse</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e12">Kozmo's Kurves</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1d">Phoenix</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e27">Twister</MDBDropdownItem>
-                      <span>Lakemont Park</span>
+                      <span className="park-name">Lakemont Park</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e13">Leap-The-Dips</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e18">Little Leaper</MDBDropdownItem>
-                      <span>Sesame Place</span>
+                      <span className="park-name">Sesame Place</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1b">Oscar's Wacky Taxi</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e28">Vapor Trail</MDBDropdownItem>
-                      <span>Waldameer</span>
+                      <span className="park-name">Waldameer</span>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e07">Comet</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1f">Ravine Flyer II</MDBDropdownItem>
                       <MDBDropdownItem href="/coasters/5d0aae19c64c9f1682257e1e">Ravinie Flyer III</MDBDropdownItem>
@@ -166,10 +166,10 @@ class Navbar extends Component {
                 <MDBDropdownToggle nav caret>
                   <MDBIcon className="nav-icon" icon="user" />
                 </MDBDropdownToggle>
-                <MDBDropdownMenu className="dropdown-default">
-                  <MDBDropdownItem href="/signin">Sign In</MDBDropdownItem>
-                  <MDBDropdownItem href="#!" onClick={this.handleSignoutSubmit}>Sign Out</MDBDropdownItem>
-                  <MDBDropdownItem href="/signup">Create a Profile</MDBDropdownItem>
+                <MDBDropdownMenu className="dropdown-default right-menu">
+                  <MDBDropdownItem className="right-menu-item" href="/signin">Sign In</MDBDropdownItem>
+                  <MDBDropdownItem className="right-menu-item" href="#!" onClick={this.handleSignoutSubmit}>Sign Out</MDBDropdownItem>
+                  <MDBDropdownItem className="right-menu-item" href="/signup">Create a Profile</MDBDropdownItem>
                 </MDBDropdownMenu>
               </MDBDropdown>
             </MDBNavItem>
